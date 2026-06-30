244f892c09d87deea4b2cbaf9d747ac9ba4731c8
Ashish Patel <96334594+ashi2222@users.noreply.github.com>
2026-06-30 23:24:32 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool dfs(int n , int a , vector<vector<int>>&p , 
+    unordered_map<int,vector<int>>&mp , vector<int>&visited , vector<int>&permanent
+    ,vector<int>&ans)
+    {
+        visited[a]=1;
+        for(auto j : mp[a])
+        {
+            if(visited[j]==1)return false;
+            if(permanent[j]==0)
+            {
+                bool ab = dfs(n,j,p,mp,visited , permanent , ans);
+                if(!ab)return false;
+            }
+        }
+        visited[a]=0;
+        permanent[a]=1;
+        ans.push_back(a);
+        return true;
+    }
+    vector<int> findOrder(int a, vector<vector<int>>& p) {
+        int n = p.size();
+        unordered_map<int,vector<int>>mp;
+        vector<int>permanent(a,0);
+        for(auto i : p)
+        {
+            int a = i[0];
+            int b = i[1];
+            mp[b].push_back(a);
+        }
+        vector<int>visited(a,0);
+        vector<int>ans;
+        for(int i =0 ; i< a ; i++)
+        {
+            if(permanent[i] == 0)
+            {
+                bool ab = dfs(a,i,p,mp,visited,permanent,ans);
+                if(!ab)return {};
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
