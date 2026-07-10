d26a07e9a02a693ff937c369205649472d319b2a
Ashish Patel <96334594+ashi2222@users.noreply.github.com>
2026-07-11 00:59:41 +0530

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                auto it = lower_bound(ans.begin() , ans.end() , nums[i]) - ans.begin();
+                ans[it]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
