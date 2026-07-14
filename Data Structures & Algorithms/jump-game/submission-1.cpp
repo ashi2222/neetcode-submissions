3fe9f942dd31aeabcc08d9339e4fdc11497c8818
Ashish Patel <96334594+ashi2222@users.noreply.github.com>
2026-07-14 19:50:17 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int n = nums.size();
+        int goal = n-1;
+        for(int i = n-2 ; i>= 0 ;i--)
+        {  
+            int j = i + nums[i];
+            if(j >= goal){
+                goal = i;
+            }
+        }
+        return goal==0;
+    }
+};
